1f8a3824c6dfb49f985ac51916eadbe5939fbb20
Julia Holadava <juliaholadava@gmail.com>
2024-04-25 12:35:57 +0200

    feat: add Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+import './Footer.css';
+
+const Footer = (): ReactElement => {
+  return (
+    <footer className="footer">
+      <p>© by Julia Holadava, 2024</p>
+    </footer>
+  );
+};
+
+export default Footer;
