ee53392bd8648873b9bd86752ecb9088d9045701
Julia Holadava <juliaholadava@gmail.com>
2024-04-30 22:25:46 +0200

    feat: define types for CarForm action and state

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,8 +1,19 @@
 export const SET_ACTIVE_TAB = 'SET_ACTIVE_TAB';
+export const SET_CAR_DETAILS = 'SET_CAR_DETAILS';
 
 export type TabName = 'garage' | 'winners';
 
 export interface SetActiveTabAction {
   type: typeof SET_ACTIVE_TAB;
   payload: TabName;
+}
+
+export interface CarDetails {
+  name: string;
+  color: string;
+}
+
+export interface SetCarDetailsAction {
+  type: typeof SET_CAR_DETAILS;
+  payload: CarDetails;
 }
