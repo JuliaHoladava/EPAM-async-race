2b956b78d5ba3ee146b13ee1c6f64e4b305572c8
Julia Holadava <juliaholadava@gmail.com>
2024-04-30 22:41:02 +0200

    feat: update Redux store with car reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import navigationReducer from './reducers/navigationSlice';
+import navigationSlice from './reducers/navigationSlice';
+import carDetailsSlice from './reducers/carDetailsSlice';
 
 const store = configureStore({
-  reducer: navigationReducer,
+  reducer: {
+    navigation: navigationSlice,
+    car: carDetailsSlice,
+  },
 });
 
 export default store;
