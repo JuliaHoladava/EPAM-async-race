ac1010f1495d3788160e571db27588356586bcf6
Julia Holadava <juliaholadava@gmail.com>
2024-05-01 21:47:52 +0200

    feat: update component with setCars action

--- src/redux/reducers/carDetailsSlice.ts
+++ src/redux/reducers/carDetailsSlice.ts
@@ -1,12 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { CarDetails } from '../types';
+import { PropsCar } from '../../types/interfaces';
 
 interface CarState {
   carDetails: CarDetails | null;
+  cars: PropsCar[];
 }
 
 const initialState: CarState = {
   carDetails: null,
+  cars: [],
 };
 
 const carDetailsSlice = createSlice({
@@ -16,8 +19,11 @@ const carDetailsSlice = createSlice({
     setCarDetails: (state, action: { payload: CarDetails }) => {
       state.carDetails = action.payload;
     },
+    setCars: (state, action: { payload: PropsCar[] }) => {
+      state.cars = action.payload;
+    },
   },
 });
 
-export const { setCarDetails } = carDetailsSlice.actions;
+export const { setCarDetails, setCars } = carDetailsSlice.actions;
 export default carDetailsSlice.reducer;
