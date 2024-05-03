0f965f0d89a5dcbcb0b56add84db4ae01d214b64
Julia Holadava <juliaholadava@gmail.com>
2024-05-03 12:56:12 +0200

    feat: update store with pagination reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import navigationSlice from './reducers/navigationSlice';
 import carDetailsSlice from './reducers/carDetailsSlice';
+import paginationSlice from './reducers/paginationSlice';
 
 const store = configureStore({
   reducer: {
     navigation: navigationSlice,
     car: carDetailsSlice,
+    pagination: paginationSlice,
   },
 });
 
