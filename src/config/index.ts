29b60f0c7e60d405c7f4a67792021b95680a508a
Julia Holadava <juliaholadava@gmail.com>
2024-05-05 10:30:53 +0200

    feat: update config object with apiEngine url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,11 @@
 interface Config {
   apiUrl: string;
+  apiEngine: string;
 }
 
 const config: Config = {
   apiUrl: 'http://localhost:3000/garage',
+  apiEngine: 'http://localhost:3000/engine?id=',
 };
 
 export default config;
