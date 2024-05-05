002be8fe94c7637ae4d7c228056a4155ed8c197f
Julia Holadava <juliaholadava@gmail.com>
2024-05-05 19:42:10 +0200

    feat: update Redux store with winner reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import navigationSlice from './reducers/navigationSlice';
 import carDetailsSlice from './reducers/carDetailsSlice';
 import paginationSlice from './reducers/paginationSlice';
+import winnerSlice from './reducers/winnerSlice';
 
 const store = configureStore({
   reducer: {
     navigation: navigationSlice,
     car: carDetailsSlice,
     pagination: paginationSlice,
+    winner: winnerSlice,
   },
 });
 
