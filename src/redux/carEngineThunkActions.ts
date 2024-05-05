098d2d8862c0eae34302e94a7c769d0c6c1b07e0
Julia Holadava <juliaholadava@gmail.com>
2024-05-05 15:29:10 +0200

    feat: implement check engine status action

--- src/redux/carEngineThunkActions.ts
+++ src/redux/carEngineThunkActions.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { startEngine } from '../api/startEngine';
 import { stopEngine } from '../api/stopEngine';
+import { checkEngineWhileDriving } from '../api/checkEngineWhileDriving';
 
 export const startCarEngine = createAsyncThunk(
   'car/startEngine',
@@ -36,6 +37,36 @@ export const stopCarEngine = createAsyncThunk(
         message: 'Engine did not stop correctly',
       });
     } catch (error) {
+      return rejectWithValue({
+        status: 'error',
+        message: 'Failed to start the engine',
+      });
+    }
+  },
+);
+
+export const checkEngineStatus = createAsyncThunk(
+  'car/checkEngineStatus',
+  async (id: number, { rejectWithValue }) => {
+    try {
+      const response = await checkEngineWhileDriving(id);
+      return response;
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(
+          `checkEngineStatus caught error for car ${id}:`,
+          error.message,
+        );
+        const errorMessage = error.message;
+        const isCriticalError = errorMessage.includes('500');
+        return rejectWithValue({
+          id: id,
+          status: isCriticalError ? 'error' : 'failed',
+          message: isCriticalError
+            ? 'Engine broken'
+            : 'Failed to start the engine',
+        });
+      }
       return rejectWithValue({
         status: 'error',
         message: 'Failed to start the engine',
