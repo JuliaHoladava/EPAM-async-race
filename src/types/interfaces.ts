1a057e56f29bd4e876b3c78833b074c3cf833971
Julia Holadava <juliaholadava@gmail.com>
2024-05-05 22:34:56 +0200

    fix: update interfaces

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,7 +1,7 @@
 export interface PropsCar {
   id: number;
-  name?: string;
-  color?: string;
+  name: string;
+  color: string;
   engineStatus?: 'idle' | 'started' | 'stopped' | 'driving' | 'error';
   velocity?: number;
 }
@@ -10,4 +10,6 @@ export interface PropsWinners {
   id: number;
   wins: number;
   time: number;
+  name: string;
+  color: string;
 }
