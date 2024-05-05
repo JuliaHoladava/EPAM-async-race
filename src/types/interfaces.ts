31279bfa086bf38b80148dec2f86002b6df97261
Julia Holadava <juliaholadava@gmail.com>
2024-05-05 19:38:29 +0200

    feat: add PropWinners interface

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -5,3 +5,9 @@ export interface PropsCar {
   engineStatus?: 'idle' | 'started' | 'stopped' | 'driving' | 'error';
   velocity?: number;
 }
+
+export interface PropsWinners {
+  id: number;
+  wins: number;
+  time: number;
+}
